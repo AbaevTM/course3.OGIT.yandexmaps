1379bf00c6d112331ca35246a38156712b3fb71c
AbaevTM <abaevtm@gmail.com>
2014-12-17 17:45:13 +0300

    1December version

--- search_yandexmap_description_builder.c
+++ search_yandexmap_description_builder.c
@@ -23,6 +23,7 @@ along with OSTIS. If not, see <http://www.gnu.org/licenses/>.
 #include "search_yandexmap_description_builder.h"
 #include "search_keynodes.h"
 #include "search_utils.h"
+#include "../merge/merge.h"
 #include "search_defines.h"
 #include <glib.h>
 #include <sc_helper.h>
@@ -92,7 +93,7 @@ sc_result add_map_object_description_into_answer(
         relocateResultString(0);
         //CHECK FOR OBJECT TYPE
         //GEO_ROUTE
-        typeIterator = sc_iterator3_f_a_f_new(
+        typeIterator = sc_iterator3_f_a_f_new(s_default_ctx,
                     concept_geo_route,
                     sc_type_arc_pos_const_perm,
                     mapObject
@@ -111,7 +112,7 @@ sc_result add_map_object_description_into_answer(
         }
         sc_iterator3_free(typeIterator);
         //POINT
-        typeIterator = sc_iterator3_f_a_f_new(
+        typeIterator = sc_iterator3_f_a_f_new(s_default_ctx,
                     concept_mapped_point,
                     sc_type_arc_pos_const_perm,
                     mapObject
@@ -130,7 +131,7 @@ sc_result add_map_object_description_into_answer(
         }
         sc_iterator3_free(typeIterator);
         //LINE STRING
-        typeIterator = sc_iterator3_f_a_f_new(
+        typeIterator = sc_iterator3_f_a_f_new(s_default_ctx,
                     concept_mapped_line_string,
                     sc_type_arc_pos_const_perm,
                     mapObject
@@ -149,7 +150,7 @@ sc_result add_map_object_description_into_answer(
         }
         sc_iterator3_free(typeIterator);
         //LINEAR RING
-        typeIterator = sc_iterator3_f_a_f_new(
+        typeIterator = sc_iterator3_f_a_f_new(s_default_ctx,
                     concept_mapped_linear_ring,
                     sc_type_arc_pos_const_perm,
                     mapObject
@@ -168,7 +169,7 @@ sc_result add_map_object_description_into_answer(
         }
         sc_iterator3_free(typeIterator);
         //POLYGON
-        typeIterator = sc_iterator3_f_a_f_new(
+        typeIterator = sc_iterator3_f_a_f_new(s_default_ctx,
                     concept_mapped_polygon,
                     sc_type_arc_pos_const_perm,
                     mapObject
@@ -197,7 +198,7 @@ void createDescriptionGeoRoute(sc_addr object){
     sc_iterator3 *routeIterator;
     sc_iterator3 *typeIterator;
     sc_addr route_element;
-    routeIterator = sc_iterator3_f_a_a_new(
+    routeIterator = sc_iterator3_f_a_a_new(s_default_ctx,
                 object,
                 sc_type_arc_pos_const_perm,
                 sc_type_const | sc_type_node
@@ -205,7 +206,7 @@ void createDescriptionGeoRoute(sc_addr object){
     while( sc_iterator3_next(routeIterator)){
         route_element = sc_iterator3_value(routeIterator,2);
         //POINT
-        typeIterator = sc_iterator3_f_a_f_new(
+        typeIterator = sc_iterator3_f_a_f_new(s_default_ctx,
                     concept_mapped_point,
                     sc_type_arc_pos_const_perm,
                     route_element
@@ -218,7 +219,7 @@ void createDescriptionGeoRoute(sc_addr object){
         }
         sc_iterator3_free(typeIterator);
         //LINE STRING
-        typeIterator = sc_iterator3_f_a_f_new(
+        typeIterator = sc_iterator3_f_a_f_new(s_default_ctx,
                     concept_mapped_line_string,
                     sc_type_arc_pos_const_perm,
                     route_element
@@ -231,7 +232,7 @@ void createDescriptionGeoRoute(sc_addr object){
         }
         sc_iterator3_free(typeIterator);
         //LINEAR RING
-        typeIterator = sc_iterator3_f_a_f_new(
+        typeIterator = sc_iterator3_f_a_f_new(s_default_ctx,
                     concept_mapped_linear_ring,
                     sc_type_arc_pos_const_perm,
                     route_element
@@ -244,7 +245,7 @@ void createDescriptionGeoRoute(sc_addr object){
         }
         sc_iterator3_free(typeIterator);
         //POLYGON
-        typeIterator = sc_iterator3_f_a_f_new(
+        typeIterator = sc_iterator3_f_a_f_new(s_default_ctx,
                     concept_mapped_polygon,
                     sc_type_arc_pos_const_perm,
                     route_element
@@ -287,7 +288,7 @@ void describeCoordinatesPoint(sc_addr object){
     sc_addr coordinates;
     sc_addr coordinate;
     writeToResultString("\n\t\"coordinates\" : ");
-    coordinatesIterator = sc_iterator5_a_a_f_a_f_new(
+    coordinatesIterator = sc_iterator5_a_a_f_a_f_new(s_default_ctx,
                 sc_type_node | sc_type_const,
                 sc_type_arc_common | sc_type_const,
                 object,
@@ -296,7 +297,7 @@ void describeCoordinatesPoint(sc_addr object){
                 );
     if( sc_iterator5_next(coordinatesIterator)){
         coordinates = sc_iterator5_value(coordinatesIterator, 0);
-        coordinateIterator = sc_iterator3_f_a_a_new(
+        coordinateIterator = sc_iterator3_f_a_a_new(s_default_ctx,
                     coordinates,
                     sc_type_arc_pos_const_perm,
                     sc_type_const | sc_type_node
@@ -316,7 +317,7 @@ void describeCoordinatesPolygon(sc_addr object){
     sc_addr coordinates;
     sc_addr coordinate;
     writeToResultString("\n\t\"coordinates\" : [");
-    coordinatesIterator = sc_iterator5_a_a_f_a_f_new(
+    coordinatesIterator = sc_iterator5_a_a_f_a_f_new(s_default_ctx,
                 sc_type_node | sc_type_const,
                 sc_type_arc_common | sc_type_const,
                 object,
@@ -327,7 +328,7 @@ void describeCoordinatesPolygon(sc_addr object){
         coordinates = sc_iterator5_value(coordinatesIterator, 0);
         //exterior border
         writeToResultString("\n\t\t[");
-        coordinateIterator = sc_iterator5_f_a_a_a_f_new(
+        coordinateIterator = sc_iterator5_f_a_a_a_f_new(s_default_ctx,
                     coordinates,
                     sc_type_arc_pos_const_perm,
                     sc_type_const | sc_type_node,
@@ -343,7 +344,7 @@ void describeCoordinatesPolygon(sc_addr object){
         writeToResultString("\n\t\t],");
         //interior border
         writeToResultString("\n\t\t[");
-        coordinateIterator = sc_iterator5_f_a_a_a_f_new(
+        coordinateIterator = sc_iterator5_f_a_a_a_f_new(s_default_ctx,
                     coordinates,
                     sc_type_arc_pos_const_perm,
                     sc_type_const | sc_type_node,
@@ -368,7 +369,7 @@ void describeCoordinatesLine(sc_addr object){
     sc_addr coordinates;
     sc_addr coordinate;
     writeToResultString("\n\t\"coordinates\" : [");
-    coordinatesIterator = sc_iterator5_a_a_f_a_f_new(
+    coordinatesIterator = sc_iterator5_a_a_f_a_f_new(s_default_ctx,
                 sc_type_node | sc_type_const,
                 sc_type_arc_common | sc_type_const,
                 object,
@@ -377,7 +378,7 @@ void describeCoordinatesLine(sc_addr object){
                 );
     if( sc_iterator5_next(coordinatesIterator)){
         coordinates = sc_iterator5_value(coordinatesIterator, 0);
-        coordinateIterator = sc_iterator3_f_a_a_new(
+        coordinateIterator = sc_iterator3_f_a_a_new(s_default_ctx,
                     coordinates,
                     sc_type_arc_pos_const_perm,
                     sc_type_const | sc_type_node
@@ -401,7 +402,7 @@ void describeCoordinate( sc_addr coordinate){
     sc_char * contentBuffer;
     size_t buferLength = 30;
     sc_uint32 contentBytesRead;
-    wgs84TranslationIterator = sc_iterator5_f_a_a_a_f_new(
+    wgs84TranslationIterator = sc_iterator5_f_a_a_a_f_new(s_default_ctx,
                 coordinate,
                 sc_type_arc_common | sc_type_const,
                 sc_type_const | sc_type_node,
@@ -412,7 +413,7 @@ void describeCoordinate( sc_addr coordinate){
         wgs84Translation = sc_iterator5_value(wgs84TranslationIterator, 2);
         //Format : latitude , longitude
         //latitude
-        latitudeIterator = sc_iterator5_f_a_a_a_f_new(
+        latitudeIterator = sc_iterator5_f_a_a_a_f_new(s_default_ctx,
                     wgs84Translation,
                     sc_type_arc_common | sc_type_const,
                     sc_type_link,
@@ -422,7 +423,7 @@ void describeCoordinate( sc_addr coordinate){
         if( sc_iterator5_next(latitudeIterator)){
             latitude = sc_iterator5_value(latitudeIterator, 2);
             writeToResultString("\n\t\t[");
-            sc_memory_get_link_content(latitude,&contentStream);
+            sc_memory_get_link_content(s_default_ctx,latitude,&contentStream);
             contentBuffer = calloc(buferLength,sizeof(sc_char));
             sc_stream_read_data(contentStream,contentBuffer,buferLength,&contentBytesRead);
             writeToResultString(contentBuffer);
@@ -432,7 +433,7 @@ void describeCoordinate( sc_addr coordinate){
         }
         sc_iterator5_free(latitudeIterator);
         //longitude
-        longitudeIterator = sc_iterator5_f_a_a_a_f_new(
+        longitudeIterator = sc_iterator5_f_a_a_a_f_new(s_default_ctx,
                     wgs84Translation,
                     sc_type_arc_common | sc_type_const,
                     sc_type_link,
@@ -441,7 +442,7 @@ void describeCoordinate( sc_addr coordinate){
                     );
         if( sc_iterator5_next(longitudeIterator)){
             longitude = sc_iterator5_value(longitudeIterator, 2);
-            sc_memory_get_link_content(longitude,&contentStream);
+            sc_memory_get_link_content(s_default_ctx,longitude,&contentStream);
             contentBuffer = calloc(buferLength,sizeof(sc_char));
             sc_stream_read_data(contentStream,contentBuffer,buferLength,&contentBytesRead);
             writeToResultString(contentBuffer);
@@ -456,23 +457,23 @@ void describeCoordinate( sc_addr coordinate){
 
 sc_addr createDescriptionNode(sc_addr mapObject){
     sc_addr formatTuple;
-    sc_addr translatedNode = sc_memory_link_new();
-    sc_addr translatedNodeTuple = sc_memory_arc_new(
+    sc_addr translatedNode = sc_memory_link_new(s_default_ctx);
+    sc_addr translatedNodeTuple = sc_memory_arc_new(s_default_ctx,
                 sc_type_const|sc_type_arc_common,
                 mapObject,
                 translatedNode
                 );
-    sc_memory_arc_new(
+    sc_memory_arc_new(s_default_ctx,
                 sc_type_arc_pos_const_perm,
                 nrel_yandex_map,
                 translatedNodeTuple
                 );
-    formatTuple = sc_memory_arc_new(
+    formatTuple = sc_memory_arc_new(s_default_ctx,
                 sc_type_const|sc_type_arc_common,
                 translatedNode,
                 format_yandexmaplink
                 );
-    sc_memory_arc_new(
+    sc_memory_arc_new(s_default_ctx,
                 sc_type_arc_pos_const_perm,
                 nrel_format,
                 formatTuple
@@ -482,15 +483,15 @@ sc_addr createDescriptionNode(sc_addr mapObject){
 
 void appendDescriptionNodeToAnswer(sc_addr translatedNode, sc_addr answer){
     sc_addr arc;
-    if (sc_helper_check_arc(answer, translatedNode, sc_type_arc_pos_const_perm) == SC_TRUE)
+    if (sc_helper_check_arc(s_default_ctx,answer, translatedNode, sc_type_arc_pos_const_perm) == SC_TRUE)
         return;
-    arc = sc_memory_arc_new(sc_type_arc_pos_const_perm, answer, translatedNode);
+    arc = sc_memory_arc_new(s_default_ctx,sc_type_arc_pos_const_perm, answer, translatedNode);
     SYSTEM_ELEMENT(arc);
 }
 
 sc_bool checkIfMapHasBeenBuilt(sc_addr object){
     sc_iterator5 * mapIterator;
-    mapIterator = sc_iterator5_f_a_a_a_f_new(
+    mapIterator = sc_iterator5_f_a_a_a_f_new(s_default_ctx,
                 object,
                 sc_type_arc_common | sc_type_const,
                 sc_type_link,
@@ -555,7 +556,7 @@ void flushStringToDescriptionNode(sc_addr descriptionNode){
     }
     sc_stream * stream = sc_stream_memory_new( temp, strlen(resultString),
                                                SC_STREAM_READ, SC_FALSE);
-    sc_memory_set_link_content(descriptionNode,stream);
+    sc_memory_set_link_content(s_default_ctx,descriptionNode,stream);
     free(temp);
     sc_stream_free(stream);
     free(resultString);
@@ -565,112 +566,112 @@ void flushStringToDescriptionNode(sc_addr descriptionNode){
 }
 
 sc_result initializeYandexmapDescriptionConstants(){
-    if(sc_helper_find_element_by_system_identifier(
+    if(sc_helper_find_element_by_system_identifier(s_default_ctx,
                 CONCEPT_MAPPED_POINT,
                 strlen(CONCEPT_MAPPED_POINT),
                 &concept_mapped_point
                 ) != SC_RESULT_OK) {
         return SC_RESULT_ERROR;
     }
-    if(sc_helper_find_element_by_system_identifier(
+    if(sc_helper_find_element_by_system_identifier(s_default_ctx,
                 CONCEPT_MAPPED_LINE_STRING,
                 strlen(CONCEPT_MAPPED_LINE_STRING),
                 &concept_mapped_line_string
                 ) != SC_RESULT_OK) {
         return SC_RESULT_ERROR;
     }
-    if(sc_helper_find_element_by_system_identifier(
+    if(sc_helper_find_element_by_system_identifier(s_default_ctx,
                 CONCEPT_MAPPED_LINEAR_RING,
                 strlen(CONCEPT_MAPPED_LINEAR_RING),
                 &concept_mapped_linear_ring
                 ) != SC_RESULT_OK) {
         return SC_RESULT_ERROR;
     }
-    if(sc_helper_find_element_by_system_identifier(
+    if(sc_helper_find_element_by_system_identifier(s_default_ctx,
                 CONCEPT_MAPPED_POLYGON,
                 strlen(CONCEPT_MAPPED_POLYGON),
                 &concept_mapped_polygon
                 ) != SC_RESULT_OK) {
         return SC_RESULT_ERROR;
     }
-    if(sc_helper_find_element_by_system_identifier(
+    if(sc_helper_find_element_by_system_identifier(s_default_ctx,
                 NREL_YANDEX_MAP,
                 strlen(NREL_YANDEX_MAP),
                 &nrel_yandex_map
                 ) != SC_RESULT_OK) {
         return SC_RESULT_ERROR;
     }
-    if(sc_helper_find_element_by_system_identifier(
+    if(sc_helper_find_element_by_system_identifier(s_default_ctx,
                 NREL_FORMAT,
                 strlen(NREL_FORMAT),
                 &nrel_format
                 ) != SC_RESULT_OK) {
         return SC_RESULT_ERROR;
     }
-    if(sc_helper_find_element_by_system_identifier(
+    if(sc_helper_find_element_by_system_identifier(s_default_ctx,
                 FORMAT_YANDEXMAPLINK,
                 strlen(FORMAT_YANDEXMAPLINK),
                 &format_yandexmaplink
                 ) != SC_RESULT_OK) {
         return SC_RESULT_ERROR;
     }
-    if(sc_helper_find_element_by_system_identifier(
+    if(sc_helper_find_element_by_system_identifier(s_default_ctx,
                 NREL_GEODETIC_COORDINATES,
                 strlen(NREL_GEODETIC_COORDINATES),
                 &nrel_geodetic_coordinates
                 ) != SC_RESULT_OK) {
         return SC_RESULT_ERROR;
     }
-    if(sc_helper_find_element_by_system_identifier(
+    if(sc_helper_find_element_by_system_identifier(s_default_ctx,
                 NREL_WGS_84_TRANSLATION,
                 strlen(NREL_WGS_84_TRANSLATION),
                 &nrel_WGS_84_translation
                 ) != SC_RESULT_OK) {
         return SC_RESULT_ERROR;
     }
-    if(sc_helper_find_element_by_system_identifier(
+    if(sc_helper_find_element_by_system_identifier(s_default_ctx,
                 NREL_WGS_84_LONGITUDE_DEGREE,
                 strlen(NREL_WGS_84_LONGITUDE_DEGREE),
                 &nrel_WGS_84_longitude_degree
                 ) != SC_RESULT_OK) {
         return SC_RESULT_ERROR;
     }
-    if(sc_helper_find_element_by_system_identifier(
+    if(sc_helper_find_element_by_system_identifier(s_default_ctx,
                 NREL_WGS_84_LATITUDE_DEGREE,
                 strlen(NREL_WGS_84_LATITUDE_DEGREE),
                 &nrel_WGS_84_latitude_degree
                 ) != SC_RESULT_OK) {
         return SC_RESULT_ERROR;
     }
-    if(sc_helper_find_element_by_system_identifier(
+    if(sc_helper_find_element_by_system_identifier(s_default_ctx,
                 RREL_EXTERIOR_POLYGON_BORDER,
                 strlen(RREL_EXTERIOR_POLYGON_BORDER),
                 &rrel_exterior_polygon_border
                 ) != SC_RESULT_OK) {
         return SC_RESULT_ERROR;
     }
-    if(sc_helper_find_element_by_system_identifier(
+    if(sc_helper_find_element_by_system_identifier(s_default_ctx,
                 RREL_INTERIOR_POLYGON_BORDER,
                 strlen(RREL_INTERIOR_POLYGON_BORDER),
                 &rrel_interior_polygon_border
                 ) != SC_RESULT_OK) {
         return SC_RESULT_ERROR;
     }
-    if(sc_helper_find_element_by_system_identifier(
+    if(sc_helper_find_element_by_system_identifier(s_default_ctx,
                 CONCEPT_GEO_ROUTE,
                 strlen(CONCEPT_GEO_ROUTE),
                 &concept_geo_route
                 ) != SC_RESULT_OK) {
         return SC_RESULT_ERROR;
     }
-    if(sc_helper_find_element_by_system_identifier(
+    if(sc_helper_find_element_by_system_identifier(s_default_ctx,
                 RREL_GEO_ROUTE_BEGIN,
                 strlen(RREL_GEO_ROUTE_BEGIN),
                 &rrel_geo_route_begin
                 ) != SC_RESULT_OK) {
         return SC_RESULT_ERROR;
     }
-    if(sc_helper_find_element_by_system_identifier(
+    if(sc_helper_find_element_by_system_identifier(s_default_ctx,
                 RREL_GEO_ROUTE_END,
                 strlen(RREL_GEO_ROUTE_END),
                 &rrel_geo_route_end
